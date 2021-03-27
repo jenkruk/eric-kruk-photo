1c83d1193c7dfb492b1ea3cab55e8417786d7a67
Jennifer Kruk <jenkruk@gmail.com>
2021-03-27 14:56:11 -0700

    finished about page template

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,9 +1,51 @@
 import React from 'react'
 
 export const About = () => {
+
     return (
         <div>
-            <h3>About</h3>
+            <div className="aboutRow row">
+            <div className="aboutImg"></div>
+            <div className="paragraph">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ducimus fuga reiciendis maxime! Distinctio eveniet debitis incidunt nam provident nemo sunt dolor veritatis voluptas accusantium optio, saepe ea autem ipsum in voluptates, pariatur consequatur. Esse adipisci vitae inventore eum dolor corporis, amet odit commodi quaerat illum, animi deserunt aspernatur mollitia corrupti quia ducimus ipsa atque tempore eius non molestiae nam. Aliquam similique quidem ab, unde quisquam quae omnis mollitia deserunt molestias eius consequatur cum amet perspiciatis illo nemo, voluptatibus facilis. </p>
+                <br />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ducimus fuga reiciendis maxime! Distinctio eveniet debitis incidunt nam provident nemo sunt dolor veritatis voluptas accusantium optio, saepe ea autem ipsum in voluptates, pariatur consequatur. Esse adipisci vitae inventore eum dolor corporis, amet odit commodi quaerat illum, animi deserunt aspernatur mollitia corrupti quia ducimus ipsa atque tempore eius non molestiae nam. Aliquam similique quidem ab, unde quisquam quae omnis mollitia deserunt molestias eius consequatur cum amet perspiciatis illo nemo, voluptatibus facilis. </p>
+            </div>
+            </div>
+
+            <div className="published">As seen in:</div>
+            <div className="publishedRow">
+                <div className="luxe">
+                <a href="https://luxesource.com/simplicity-defines-an-arizona-couples-next-chapter-in-life-laurel-pfannenstiel/#.YF-iiEjYqL4" target="_blank" id="luxeLink">
+                    <h4>Luxe Magazine</h4>
+                    <img className="luxeThumbnail" src="https://luxesource.com/wp-content/uploads/2021/01/LX_Arizona54_HOM_Pfannenstiel_07-scaled.jpg" alt="luxe thumbnail"/>
+                </a>
+                </div>
+                <div className="luxe">
+                <a href="https://luxesource.com/simplicity-defines-an-arizona-couples-next-chapter-in-life-laurel-pfannenstiel/#.YF-iiEjYqL4" target="_blank" id="luxeLink">
+                    <h4>Luxe Magazine</h4>
+                    <img className="luxeThumbnail" src="https://luxesource.com/wp-content/uploads/2021/01/LX_Arizona54_HOM_Pfannenstiel_07-scaled.jpg" alt="luxe thumbnail"/>
+                </a>
+                </div>
+                <div className="luxe">
+                <a href="https://luxesource.com/simplicity-defines-an-arizona-couples-next-chapter-in-life-laurel-pfannenstiel/#.YF-iiEjYqL4" target="_blank" id="luxeLink">
+                    <h4>Luxe Magazine</h4>
+                    <img className="luxeThumbnail" src="https://luxesource.com/wp-content/uploads/2021/01/LX_Arizona54_HOM_Pfannenstiel_07-scaled.jpg" alt="luxe thumbnail"/>
+                </a>
+                </div>
+                <div className="luxe">
+                <a href="https://luxesource.com/simplicity-defines-an-arizona-couples-next-chapter-in-life-laurel-pfannenstiel/#.YF-iiEjYqL4" target="_blank" id="luxeLink">
+                    <h4>Luxe Magazine</h4>
+                    <img className="luxeThumbnail" src="https://luxesource.com/wp-content/uploads/2021/01/LX_Arizona54_HOM_Pfannenstiel_07-scaled.jpg" alt="luxe thumbnail"/>
+                </a>
+                </div>
+                <div className="luxe">
+                <a href="https://luxesource.com/simplicity-defines-an-arizona-couples-next-chapter-in-life-laurel-pfannenstiel/#.YF-iiEjYqL4" target="_blank" id="luxeLink">
+                    <h4>Luxe Magazine</h4>
+                    <img className="luxeThumbnail" src="https://luxesource.com/wp-content/uploads/2021/01/LX_Arizona54_HOM_Pfannenstiel_07-scaled.jpg" alt="luxe thumbnail"/>
+                </a>
+                </div>
+            </div>
         </div>
     )
 }
